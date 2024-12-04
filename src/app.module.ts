cd42c8e69bf247fba6af7ce6cdbaed7fba6aaa03
RomanVintoniak <romanvintoniak03@gmail.com>
2024-12-04 22:37:29 +0200

    Add needed dependencies and files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { StudentsModule } from './students/students.module';
+import { CronJobsService } from './cron-jobs/cron-jobs.service';
 
 @Module({
-  imports: [],
+  imports: [StudentsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    CronJobsService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
